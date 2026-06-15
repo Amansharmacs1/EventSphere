9bfe879f377fc3598e0dcc7892700987833a037c
Aman <amansharma@Amans-MacBook-Air-4.local>
2026-06-15 14:08:26 +0530

    fix: import useState to prevent React crash in student dashboard

--- client/src/pages/student/StudentDashboard.jsx
+++ client/src/pages/student/StudentDashboard.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getRecommendations } from '../../features/events/eventSlice';
 import { Link } from 'react-router-dom';
